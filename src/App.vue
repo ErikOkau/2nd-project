e0c1db6743c42dc624f2b35e747bd7cf5e4bbfad
ErikOkau <erikmsorheim@yahoo.no>
2023-08-01 11:18:15 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import navbar from './components/navbar.vue'
+import buttons from './components/buttons.vue'
+import footer from './components/footer.vue'
+
+</script>
+
+<template>
+<navbar />
+
+  <div class="main">
+    <RouterView />
+  </div>
+
+<footer />
+</template>
+
+<style scoped>
+
+</style>
